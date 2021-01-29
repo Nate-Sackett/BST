6a9f062a3169f214095c8015c104c41043283fac
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-01-28 22:49:54 -0600

    Add push (pushes node to BST)

--- BST.js
+++ BST.js
@@ -23,6 +23,47 @@ class BST {
             throw new Error("Key of BST node must be a number!");
         }
     }
+
+    /**
+     * Pushes a BST node to the BST.
+     * @param {BST} node The BST node to be added.
+     * @throws Error when node isn't a BST node or node.value isn't a unique number.
+     */
+    push(node) {
+        // Type validation (node must be a BST node)
+        if ( node !== null && node !== undefined && !node.hasOwnProperty("key") && typeof node.key === "number" 
+        && !node.hasOwnProperty("value") && !node.hasOwnProperty("left") && !node.hasOwnProperty("right") ) {
+            throw new Error("Only valid for BST nodes.");
+        }
+
+        // Check that the key is unique
+        if (this.key === node.key)
+            throw new Error(node.key + " is not a unique key.");
+
+        // Left child
+        if (node.key < this.key) {
+
+            // Try adding as left child (base case = no left child)
+            if (this.left === null)
+                this.left = node;
+
+            // Recursive case
+            else
+                this.left.push(node);
+
+        }
+        // Right child
+        else { // (this.key < node.key)
+
+            // Try adding as right child (base case = no right child)
+            if (this.right === null)
+                this.right = node;
+            
+            // Recursive case
+            else
+                this.right.push(node);
+        }
+    }
 }
 
 module.exports = BST;
