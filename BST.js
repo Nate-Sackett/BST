d095c0e0f7e28b5fff2ca07aad0a486cef8390ed
Nate-Sackett <Nate.A.Sackett@gmail.com>
2021-02-02 18:51:13 -0600

    Add balance method

--- BST.js
+++ BST.js
@@ -1,8 +1,8 @@
 /**
- * Basic (non self-balancing) recursive implementation of a Binary Search Tree with CRUD features
+ * Basic (not self-balancing) recursive implementation of a Binary Search Tree
  * @author Nate Sackett
  */
-class BST {
+module.exports = class BST {
     /**
      * Constructs a BST node with specified numerical key, any value, and null children.
      * - BST is NOT self-balancing
@@ -75,14 +75,191 @@ class BST {
         }
     }
 
+    
+    /**
+     * Finds and returns a value from the BST, given the matching key.
+     * @param {number} key The key matching the desired value.
+     * @returns Value of matching key or undefined if the key DNE.
+     */
+    getValue(key) {
+
+        // Reference to the current node in the BST
+        let curr = this;
+
+        // Loop until a match is found or DNE
+        while(curr.key !== key) {
+
+            // Try to iterate left
+            if (key < curr.key) {
+    
+                // Check for valid left child (exit condition)
+                if (curr.left === null)
+                    return undefined;
+    
+                // Iterate left
+                curr = curr.left;
+    
+            // Try to iterate right
+            } else { // curr.key < key
+    
+                // Check for valid right child (exit condition)
+                if (curr.right === null)
+                    return undefined;
+                
+                // Iterate right
+                curr = curr.right;
+            }
+        }
+
+        // Return value from matching key
+        return curr.value;
+    }
+
+
+    /**
+     * Finds and updates a value from the BST, given the matching key.
+     * @param {number} key The key matching the node to be updated.
+     * @returns The old value of the node, or undefined if the key was not found.
+     */
+    update(key, newValue) {
+
+        // Reference to the current node in the BST
+        let curr = this;
+
+        // Loop until a match is found or DNE
+        while(curr.key !== key) {
+
+            // Try to iterate left
+            if (key < curr.key) {
+    
+                // Check for valid left child (exit condition)
+                if (curr.left === null)
+                    return undefined;
+    
+                // Iterate left
+                curr = curr.left;
+    
+            // Try to iterate right
+            } else { // curr.key < key
+    
+                // Check for valid right child (exit condition)
+                if (curr.right === null)
+                    return undefined;
+                
+                // Iterate right
+                curr = curr.right;
+            }
+        }
+
+        // Return matching key after finding match
+        const temp = curr.value;
+        curr.value = newValue;
+        return temp;
+    }
+
+
+    /**
+     * Outputs sorted array of BST values (only)
+     * - Array sorted from smallest to largest key value
+     * @returns Sorted array of BST values
+     * @example (BST) root.toArray := [ 'val_4', 'val_5' ]
+     */
+    toArray() {
+        let arr = [];
+
+        // Recursive helper function
+        function toArrayHelper(curr) {
+
+            // Evaluate left child first
+            if (curr.left !== null)
+                toArrayHelper(curr.left);
+            
+            // Push self once left child fully evaluated
+            arr.push(curr.value);
+    
+            // Evaluate right child after self
+            if (curr.right !== null)
+                toArrayHelper(curr.right);
+        }
+
+        // Call (private) recursive helper function
+        toArrayHelper(this);
+
+        // Return sorted array of values
+        return arr;
+    }
+
+
+    /**
+     * Outputs sorted BST of key value pairs as an array of objects.
+     * - Sorted from smallest to largest key value.
+     * @returns Sorted array of key/value objects
+     * @example (BST) root.toKVArray := [ { key: 4, value: 'val_4' }, { key: 5, value: 'val_5' } ]
+     */
+    toKVArray() {
+        let arr = [];
+
+        // Recursive helper function
+        function toKVArrayHelper(curr) {
+
+            // Evaluate left child first
+            if (curr.left !== null)
+                toKVArrayHelper(curr.left);
+            
+            // Push self once left child fully evaluated
+            arr.push({key: curr.key, value: curr.value});
+    
+            // Evaluate right child after self
+            if (curr.right !== null)
+                toKVArrayHelper(curr.right);
+        }
+
+        // Call (private) recursive helper function
+        toKVArrayHelper(this);
+
+        // Return array of key, value pair objects
+        return arr;
+    }
+
+
+    /**
+     * Outputs sorted array of all BST keys
+     * @example (BST) root.getKeys := [ 4, 5 ]
+     * @returns Sorted array of BST keys
+     */
+    getKeys() {
+        let arr = [];
+
+        // Recursive helper function
+        function getKeyHelper(curr) {
+
+            // Evaluate left child first
+            if (curr.left !== null)
+                getKeyHelper(curr.left);
+            
+            // Push self once left child fully evaluated
+            arr.push(curr.key);
+    
+            // Evaluate right child after self
+            if (curr.right !== null)
+                getKeyHelper(curr.right);
+        }
+
+        // Call (private) recursive helper function
+        getKeyHelper(this);
+
+        // Return array of key, value pair objects
+        return arr;
+    }
+
 
     /**
      * Deletes a BST node given a valid key, then returns the key and value inside an object.
      * Does nothing and returns null given an invalid (numerical) key.
      * @param {number} key The key of the BST node to be deleted.
      * @throws Error ("Key of BST node must be a number!") when provided a key that is not a number.
-     * @example root.delete(5); // If successful, returns: { key: 5, value: {associated value} }, else returns: null
      * @returns key/value pair inside an object if node removed, or null if node does not exist.
+     * @example root.delete(5); // If successful, returns: { key: 5, value: {associated value} }, else returns: null
      */
     delete(key) {
         // Input validation
@@ -197,180 +374,49 @@ class BST {
 
     
     /**
-     * Finds and returns a value from the BST, given the matching key.
-     * @param {number} key The key matching the desired value.
-     * @returns Value of matching key or undefined if the key DNE.
+     * Converts this BST into a balanced BST (the height difference between leaf nodes is at most 1)
+     * - Distributes nodes as evenly as possible such that the height of the tree is log_2(Number of Nodes) rounded up.
+     * @returns {BST} Returns the root node of a balanced BST with the data of the input tree.
+     * @example root = root.balance(); // Overwrites root with a balanced version of the same tree.           
+     * - Input:  5      Output:  5
+     * -        / \             / \
+     * -       4   6           3   7
+     * -      /     \         / \ / \
+     * -     3       7       2  4 6  8
+     * -    /         \
+     * -   2           8
      */
-    getValue(key) {
+    balance() {
 
-        // Reference to the current node in the BST
-        let curr = this;
+        // Sort BST into array
+        const sorted = this.toKVArray();
 
-        // Loop until a match is found or DNE
-        while(curr.key !== key) {
+        // Get BST from sorted array
+        return balanceHelper(0, sorted.length-1);
 
-            // Try to iterate left
-            if (key < curr.key) {
-    
-                // Check for valid left child (exit condition)
-                if (curr.left === null)
-                    return undefined;
-    
-                // Iterate left
-                curr = curr.left;
-    
-            // Try to iterate right
-            } else { // curr.key < key
-    
-                // Check for valid right child (exit condition)
-                if (curr.right === null)
-                    return undefined;
-                
-                // Iterate right
-                curr = curr.right;
-            }
+        /**
+         * Recursive helper function
+         * @param {number} first The first index
+         * @param {number} last The last index
+         */
+        function balanceHelper(first, last) {
+
+            // Base case: first and last index have crossed
+            if (first > last)
+                return null;
+
+            // Calculate midpoint (rounding up)
+            const m = Math.ceil( (last - first)/2 ) + first;
+
+            // Make a new node
+            const newNode = new BST(sorted[m].key, sorted[m].value);
+
+            // Recursively define left and right subtrees
+            newNode.left = balanceHelper(first, m-1);
+            newNode.right = balanceHelper(m+1, last);
+
+            // Return completed tree
+            return newNode;
         }
-
-        // Return value from matching key
-        return curr.value;
-    }
-
-
-    /**
-     * Finds and updates a value from the BST, given the matching key.
-     * @param {number} key The key matching the node to be updated.
-     * @returns The old value of the node, or undefined if the key was not found.
-     */
-    update(key, newValue) {
-
-        // Reference to the current node in the BST
-        let curr = this;
-
-        // Loop until a match is found or DNE
-        while(curr.key !== key) {
-
-            // Try to iterate left
-            if (key < curr.key) {
-    
-                // Check for valid left child (exit condition)
-                if (curr.left === null)
-                    return undefined;
-    
-                // Iterate left
-                curr = curr.left;
-    
-            // Try to iterate right
-            } else { // curr.key < key
-    
-                // Check for valid right child (exit condition)
-                if (curr.right === null)
-                    return undefined;
-                
-                // Iterate right
-                curr = curr.right;
-            }
-        }
-
-        // Return matching key after finding match
-        const temp = curr.value;
-        curr.value = newValue;
-        return temp;
-    }
-
-
-    /**
-     * Outputs sorted array of BST values (only)
-     * - Array sorted from smallest to largest key value
-     * @example (BST) root.toArray := [ 'val_4', 'val_5' ]
-     * @returns Sorted array of BST values
-     */
-    toArray() {
-        let arr = [];
-
-        // Recursive helper function
-        function toArrayHelper(curr) {
-
-            // Evaluate left child first
-            if (curr.left !== null)
-                toArrayHelper(curr.left);
-            
-            // Push self once left child fully evaluated
-            arr.push(curr.value);
-    
-            // Evaluate right child after self
-            if (curr.right !== null)
-                toArrayHelper(curr.right);
-        }
-
-        // Call (private) recursive helper function
-        toArrayHelper(this);
-
-        // Return sorted array of values
-        return arr;
-    }
-
-
-    /**
-     * Outputs sorted BST of key value pairs as an array of objects.
-     * - Sorted from smallest to largest key value.
-     * @example (BST) root.toKVArray := [ { key: 4, value: 'val_4' }, { key: 5, value: 'val_5' } ]
-     * @returns Sorted array of key/value objects
-     */
-    toKVArray() {
-        let arr = [];
-
-        // Recursive helper function
-        function toKVArrayHelper(curr) {
-
-            // Evaluate left child first
-            if (curr.left !== null)
-                toKVArrayHelper(curr.left);
-            
-            // Push self once left child fully evaluated
-            arr.push({key: curr.key, value: curr.value});
-    
-            // Evaluate right child after self
-            if (curr.right !== null)
-                toKVArrayHelper(curr.right);
-        }
-
-        // Call (private) recursive helper function
-        toKVArrayHelper(this);
-
-        // Return array of key, value pair objects
-        return arr;
-    }
-
-
-    /**
-     * Outputs sorted array of all BST keys
-     * @example (BST) root.getKeys := [ 4, 5 ]
-     * @returns Sorted array of BST keys
-     */
-    getKeys() {
-        let arr = [];
-
-        // Recursive helper function
-        function getKeyHelper(curr) {
-
-            // Evaluate left child first
-            if (curr.left !== null)
-                getKeyHelper(curr.left);
-            
-            // Push self once left child fully evaluated
-            arr.push(curr.key);
-    
-            // Evaluate right child after self
-            if (curr.right !== null)
-                getKeyHelper(curr.right);
-        }
-
-        // Call (private) recursive helper function
-        getKeyHelper(this);
-
-        // Return array of key, value pair objects
-        return arr;
     }
 }
-
-module.exports = BST;
